72bbc522358f3a0dd74d4230cf7538dabf1fad41
marawan salman <marawansalman98@gmail.com>
2020-06-28 06:58:45 +0200

    app folder

---
+++ app/Teacher/add-task/add-task.module.ts
@@ -0,0 +1,23 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { addTaskPage } from './add-task.page';
+import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
+
+import { addTaskPageRoutingModule } from './add-task-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    addTaskPageRoutingModule,
+    ReactiveFormsModule,
+    TranslateModule.forChild()
+  ],
+  declarations: [addTaskPage]
+})
+export class addTaskPageModule { }
